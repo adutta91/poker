e3e43ef5b21c1c49dbd73d24d36d363161aea71e
App Academy Student <student@appacademy.io>
2016-01-13 18:05:11 -0800

    working on hand logic

--- lib/hand.rb
+++ lib/hand.rb
@@ -53,18 +53,49 @@ class Hand
   end
 
   def find_straight
+    []
   end
 
   def find_three_of_a_kind
   end
 
   def find_two_pair
+    pairs = find_pairs
+    return nil if pairs.length != 2
+    remaining_card = @cards_in_hand - pairs.flatten
+    high_pair = 
+    [3, pairs]
+
   end
 
   def find_one_pair
+    pairs = find_pairs
+    return nil if pairs.empty?
+    raise TwoPairError if pairs.length > 1
+
+    remaining_cards = @cards_in_hand - pairs.flatten
+    high_value = find_high_card(remaining_cards)
+    pair_score = [2, pairs[0][0].value, high_value[1]]
+
   end
 
-  def find_high_card
+  def find_high_card(cards = @cards_in_hand)
+    highest_card = cards.first
+    cards.each do |card|
+      highest_card = card if card > highest_card
+    end
+    [1, highest_card.value]
+
+  end
+
+  def find_pairs
+    pairs = []
+    @cards_in_hand.each_with_index do |card1, idx1|
+      @cards_in_hand[idx1+1..-1].each_with_index do |card2, idx2|
+        pairs << [card1, card2] if card1 == card2
+      end
+    end
+    pairs
   end
 end
 
@@ -76,4 +107,7 @@ class NoCardsError < StandardError
 end
 
 class CardNotInHandError < StandardError
+end
+
+class TwoPairError < StandardError
 end
